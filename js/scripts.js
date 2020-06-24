e7be41941fa5960193ad2e5ab5ef9ad355546f79
Arterius92 <artjomvangelev@gmail.com>
2020-06-24 13:42:50 +0300

    Доделал блок service и блок recall

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 $(function () {
+  function ibg() {
+    $.each($('.ibg'), function (index, val) {
+      if ($(this).find('img').length > 0) {
+        $(this).css(
+          'background-image',
+          'url("' + $(this).find('img').attr('src') + '")'
+        );
+      }
+    });
+  }
+  ibg();
+
   $('.mini-menu').click(function () {
     $(this).toggleClass('open');
     if ($(this).hasClass('open')) {
@@ -12,19 +24,39 @@ $(function () {
     }
   });
 
-  $(window).scroll(function (e) {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 80) {
       $('.header-container').css({
-        'background-color': '#883eca',
+        background: 'rgba(136, 62, 202, 0.9)',
         'padding-top': '20px',
         'padding-bottom': '20px',
       });
     } else {
       $('.header-container').css({
-        'background-color': '',
+        background: '',
         'padding-top': '',
         'padding-bottom': '',
       });
     }
+  });
+
+  $('.services-slider').hubSlider({
+    selector: $('.services-slider__slide'),
+    button: {
+      next: $('.services-slider__next'),
+      prev: $('.services-slider__prev'),
+    },
+    auto: false,
+  });
+
+  $('.recall-slider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    variableWidth: true,
+    nextArrow: $('.recall-slider__next'),
+    prevArrow: $('.recall-slider__prev')
   });
 });
